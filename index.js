d988131328a802066914862ec17a10d09b897431
becca <rebecca.kaci@gmail.com>
2021-12-08 10:59:49 +0100

    ajout du redirect

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
 const engine = require("express-handlebars").engine
+
 app.engine("handlebars", engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
     res.render('home', {
       layout: 'main'
     })
@@ -16,8 +16,31 @@ app.get("/", (req, res) => {
 
 app.post('/form/signup', (req, res) => {
     console.log(req.body)
+    const { username, password } = req.body
+
+    // Pour redirect vers la route `/signup`
+    // Avec les paramateres url username et password
+    // On démarre la séquence de parametres avec '?'
+    // parametre_name=paramatre_value
+    // On sépare nos parametres par '&'
+    res.redirect(`/signup?username=${username}&password=${password}`)
+
+    // affichage direct du template signup avef les variables
+    // res.render('signup', {
+    //   username: req.body.username,
+    //   password: req.body.password
+    // })
+})
+
+app.get('/signup', (req, res, next) => {
+    const { username, password } = req.query
+
+    res.render('signup', {
+        username,
+        password
+    })
 })
 
 app.listen(port, () => {
-  console.log('Server started on port: ' + port)
+  console.log(`Server started on port ${port}`)
 })
